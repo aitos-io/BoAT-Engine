19d817a7cfd40483a1d57e8f69b99f3ef6a0ab82
ph <penghaonan@hotmail.com>
2025-04-17 21:13:42 +0800

    Modified: rename usdc to erc20

--- demo/demo_ethereum/demo_ethereum_erc20_transfer.c
+++ demo/demo_ethereum/demo_ethereum_erc20_transfer.c
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *****************************************************************************/
-#include "usdcTransfer.h"
+#include "erc20Transfer.h"
 #include "boatlog.h"
 
 /**
